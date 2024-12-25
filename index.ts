bc9ec916094057c9c7725e7786050680ce3c63e3
Jon Canning <jon.canning@gmail.com>
2024-12-25 20:33:10 +0000

    typescript

---
+++ index.ts
@@ -0,0 +1,68 @@
+import * as charts from './charts.js'
+import { getAccountData } from './octopus.js'
+
+const getStorageValue = (key: string) => {
+  let value = localStorage.getItem('datastar')
+  if (!value) return
+  return JSON.parse(value)[key]
+}
+
+export const getRegion = () => getStorageValue('region')
+
+const getAccount = () => getStorageValue('account')
+
+const getToken = () => getStorageValue('token')
+
+export const signOut = charts.destroy
+
+export const signIn = async (account: string, token: string) => {
+  if (account && token) {
+    document.body.style.cursor = 'wait'
+    const accountData = await getAccountData(account, token)
+    document.body.style.cursor = 'default'
+    return accountData ? true : false
+  }
+  return false
+}
+
+export const getMaxPeriodFrom = () => {
+  const periodFrom = new Date()
+  if (periodFrom.getHours() >= 16) {
+    periodFrom.setDate(periodFrom.getDate() + 1)
+  }
+  return periodFrom.toISOString().slice(0, 10)
+}
+
+export const setNextDay = (periodFromValue: string, maxPeriodFromValue: string) => {
+  const periodFrom = new Date(periodFromValue)
+  if (periodFrom < new Date(maxPeriodFromValue)) {
+    periodFrom.setDate(periodFrom.getDate() + 1)
+    return periodFrom.toISOString().slice(0, 10)
+  }
+  return periodFromValue
+}
+
+export const setPreviousDay = (periodFromValue: string) => {
+  const periodFrom = new Date(periodFromValue)
+  periodFrom.setDate(periodFrom.getDate() - 1)
+  return periodFrom.toISOString().slice(0, 10)
+}
+
+const nextRateChange = () => {
+  const now = new Date()
+  const nextInterval = new Date()
+  nextInterval.setSeconds(0)
+  nextInterval.setMinutes(now.getMinutes() < 30 ? 30 : 60)
+  return nextInterval.getTime() - now.getTime()
+}
+
+export const renderCharts = (region: string, periodFromValue: string) =>
+  charts.render(region, periodFromValue, getAccount(), getToken())
+
+export const onload = (mainElement: HTMLElement, ratesChartElement: HTMLElement, costChartElement: HTMLElement) => {
+  charts.setElements(ratesChartElement, costChartElement)
+  setTimeout(() => {
+    mainElement.dispatchEvent(new Event('reload'))
+    setInterval(() => mainElement.dispatchEvent(new Event('reload')), 1800000)
+  }, nextRateChange())
+}
---
+++ octopus.ts
@@ -0,0 +1,146 @@
+type AccountData = {
+  properties: {
+    electricity_meter_points: {
+      is_export: boolean
+      mpan: string
+      meters: { serial_number: string }[]
+    }[]
+  }[]
+}
+
+type MeterData = {
+  results: {
+    consumption: number
+    interval_end: string
+  }[]
+}
+
+export type Consumption = { [serialNumber: string]: { [period: string]: number } }
+
+type Tariffs = {
+  results: {
+    display_name: string
+    brand: string
+    links: { rel: string; href: string }[]
+  }[]
+}
+
+type TariffData = {
+  single_register_electricity_tariffs: {
+    [region: string]: { direct_debit_monthly: { links: { rel: string; href: string }[] } }
+  }
+}
+
+type RegionUnitRate = {
+  valid_from: string
+  value_inc_vat: number
+}
+
+type RegionUnitRates = {
+  results: RegionUnitRate[]
+}
+
+const apiCache: { [key: string]: string } = {}
+const apiRoot = 'https://api.octopus.energy/v1/'
+
+const getPeriodTo = (periodFrom: Date) => {
+  const periodTo = new Date(periodFrom)
+  periodTo.setDate(periodTo.getDate() + 1)
+  return periodTo
+}
+
+const getData = async <T>(path: string, token?: string): Promise<T> => {
+  const headers: HeadersInit = { 'Content-Type': 'application/json' }
+  if (token) {
+    headers.Authorization = `Basic ${btoa(token)}`
+  }
+  const cacheKey = `${path} ${JSON.stringify(headers)}`
+  if (apiCache[cacheKey]) {
+    return JSON.parse(apiCache[cacheKey]) as T
+  }
+  const response = await fetch(path, { headers: headers })
+  if (response.ok) {
+    if (Object.keys(apiCache).length >= 10) {
+      delete apiCache[Object.keys(apiCache)[0]]
+    }
+    const data = await response.json()
+    apiCache[cacheKey] = JSON.stringify(data)
+    return data as T
+  }
+  throw new Error(response.statusText)
+}
+
+export const getAccountData = async (account: string, token: string) =>
+  await getData<AccountData>(`${apiRoot}accounts/${account}/`, token)
+
+export const getMeterConsumption = async (account: string, token: string, periodFrom: Date) => {
+  const periodTo = getPeriodTo(periodFrom)
+  const accountData = await getAccountData(account, token)
+  if (!accountData) return null
+  const meterPoints = accountData.properties
+    .flatMap((property) => property.electricity_meter_points)
+    .filter((point) => !point.is_export)
+  const meterConsumption: Consumption = {}
+  const meters = meterPoints.flatMap((point) =>
+    point.meters.map((meter) => ({ mpan: point.mpan, serialNumber: meter.serial_number }))
+  )
+  await Promise.all(
+    meters.map(async ({ mpan, serialNumber }) => {
+      const meterData = await getData<MeterData>(
+        `${apiRoot}electricity-meter-points/${mpan}/meters/${serialNumber}/consumption?period_from=${periodFrom.toISOString()}&period_to=${periodTo.toISOString()}`,
+        token
+      )
+      if (meterData && meterData.results.length) {
+        meterConsumption[serialNumber] = {}
+        for (const { consumption, interval_end } of meterData.results) {
+          meterConsumption[serialNumber][interval_end] = consumption
+        }
+      }
+    })
+  )
+  return Object.keys(meterConsumption).length ? meterConsumption : null
+}
+
+export const getRates = async (region: string, periodFrom: Date) => {
+  const periodTo = getPeriodTo(periodFrom)
+  const tariffs = await getData<Tariffs>(`${apiRoot}products/`)
+  const agileTariff = tariffs.results.find(
+    (tariff) => tariff.display_name === 'Agile Octopus' && tariff.brand === 'OCTOPUS_ENERGY'
+  )
+  const agileTariffDataHref = agileTariff?.links.find((link) => link.rel === 'self')?.href
+  if (!agileTariffDataHref) {
+    return []
+  }
+  const tariffData = await getData<TariffData>(agileTariffDataHref)
+  const regionTariff = tariffData.single_register_electricity_tariffs[`_${region}`]
+  if (!regionTariff) {
+    return []
+  }
+  const regionUnitRatesHref =
+    regionTariff.direct_debit_monthly.links.find((link) => link.rel === 'standard_unit_rates')?.href +
+    `?period_from=${periodFrom.toISOString()}&period_to=${periodTo.toISOString()}`
+  if (!regionUnitRatesHref) {
+    return []
+  }
+  const regionUnitRates = await getData<RegionUnitRates>(regionUnitRatesHref)
+  return regionUnitRates.results.map(({ valid_from, value_inc_vat }) => ({
+    valid_from,
+    value_inc_vat
+  }))
+}
+
+export const getConsumptionCosts = async (rates: RegionUnitRate[], meterConsumption: Consumption | null) => {
+  const ratesMap: { [key: string]: number } = {}
+  const consumptionCosts: Consumption = {}
+  for (const rate of rates) {
+    ratesMap[rate.valid_from] = rate.value_inc_vat
+  }
+  for (const serial_number in meterConsumption) {
+    consumptionCosts[serial_number] = {}
+    for (const [period, kwh] of Object.entries(meterConsumption[serial_number])) {
+      const ratesForPeriod = ratesMap[period] || 0
+      consumptionCosts[serial_number][period] = kwh * ratesForPeriod
+    }
+  }
+  return Object.keys(consumptionCosts).length ? consumptionCosts : null
+}
