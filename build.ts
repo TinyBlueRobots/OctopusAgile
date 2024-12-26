7b9fd26a1f1eb05ddfa6e7ad385162d5801be9b8
Jon Canning <jon.canning@gmail.com>
2024-12-26 14:09:28 +0000

    build script

---
+++ build.ts
@@ -0,0 +1,12 @@
+const output = await Bun.build({
+  entrypoints: ['./index.ts'],
+  outdir: './build',
+  minify: true,
+  naming: '[dir]/[hash].[ext]'
+})
+if (output.outputs.length === 0) throw new Error('No outputs')
+const buildFile = `${output.outputs[0].hash}.js`
+const indexFile = Bun.file('./index.html')
+let index = await indexFile.text()
+index = index.replace(/index.js/g, buildFile)
+await Bun.write('./index.html', index)
